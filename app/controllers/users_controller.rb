8281af395ca8a9e70c6c1ccd0b13c086e3644d4b
azamzamy <a.zamzamy@hotmail.com>
2017-09-25 16:53:06 +0200

    Sign in & sign up pages fully working with form validation. Different visitor page & guest page. unified navigator.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,5 +12,33 @@ class UsersController < ApplicationController
 # u.avatar.url # => '/url/to/file.png'
 # u.avatar.current_path # => 'path/to/file.png'
 # u.avatar_identifier # => 'file.png'
+ def index
+  @users = User.all
+ end
+ def new
+  @user = User.new
+     end
+ 
+ def show
+ 	 @user = User.find(params[:id])
+  end
 
+def create
+    @user = User.new(user_params)
+    if @user.save
+          # @carrierwaveimages = CarrierwaveImage new("no_user_profile-pic.jpg")
+
+    	  log_in @user
+    	  flash[:success] = "Welcome ^.^ !"
+        redirect_to @user
+    else
+      render 'new'
+    end
+end
+  
+
+private
+    def user_params
+      params.require(:user).permit(:f_name, :l_name, :email, :password, :password_confirmation, :avatar, :bio)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :apps
-
+  resources :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
